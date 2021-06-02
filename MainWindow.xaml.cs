1ecbecd10b7c78606c9fb6c5b49ca51a866ce2b6
Sterling Potter <sp0ttysp0t@outlook.com>
2021-06-02 21:08:46 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_part2_gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RandomSet mySet = new RandomSet(0);
+        int seed;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CreateArrayButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySet = new RandomSet(int.Parse(ArrayLengthField.Text));
+            ArrayBox.Text = string.Join(", ", mySet.arr);
+            mySet.writeFile("2a.txt");
+            MessageBox.Show("the array has been written to the file 2a.txt and array textbox has been updated");
+        }
+
+        private void CreateSeedButton_Click(object sender, RoutedEventArgs e)
+        {
+            seed = int.Parse(SeedValueBox.Text);
+            mySet.Seed(seed);
+            ArrayBox.Text = string.Join(", ", mySet.arr);
+            MessageBox.Show("the seed has been placed in the array");
+        }
+
+        private void FindSeedButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            FindSeedBox.Text = ""+ (mySet.search(seed)+1);
+        }
+
+        private void BubbleButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySet.sortBubble();
+            BubbleBox.Text = string.Join(", ", mySet.sortedArr);
+            BubblePassBox.Text = "Passes: " + mySet.bubblePasses;
+            CompareSorts();
+        }
+
+        private void OddEvenButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySet.sortOddEven();
+            OddEvenBox.Text = string.Join(", ", mySet.sortedArr);
+            OddEvenPassBox.Text = "Passes: " + mySet.oddEvenPasses;
+            CompareSorts();
+        }
+
+        private void CompareSorts()
+        {
+            if(mySet.bubblePasses>0 && mySet.oddEvenPasses > 0)
+            {
+                ComparisonBox.Text="Odd Even sort is "+ ((float)mySet.bubblePasses / (float)mySet.oddEvenPasses * 100) + "% more efficient";
+            }
+        }
+    }
+}
---
+++ RandomSet.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment_part2_gui
+{
+    public class RandomSet
+    {
+        public int[] arr = new int[] { };
+        public int[] sortedArr = new int[] { };
+        Random randomGenerator = new Random();
+        public int bubblePasses = 0;
+        public int oddEvenPasses = 0;
+
+
+        public RandomSet(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = randomGenerator.Next(99) + 1;
+            }
+        }
+
+        public void Seed(int seed)
+        {
+            
+            //get 20% of the array size
+            int random = randomGenerator.Next(arr.Length);
+            int offset = (random) / 5+1;
+            int location = arr.Length - offset;
+            
+            //add the seed
+            arr[location] = seed;
+        }
+
+        public int search(int num)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (num == arr[i])
+                {
+                    //returns place in array
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public int[] bubbleSort(int[] data)
+        {
+            int lenD = data.Length;
+            int passes = 0;
+
+            int tmp = 0;
+            for (int i = 0; i < lenD; i++)
+            {
+                for (int j = (lenD - 1); j >= (i + 1); j--)
+                {
+                    if (data[j] < data[j - 1])
+                    {
+                        tmp = data[j];
+                        data[j] = data[j - 1];
+                        data[j - 1] = tmp;
+                    }
+                }
+                passes++;
+            }
+            bubblePasses = passes;
+            return data;
+        }
+
+        public void sortBubble()
+        {
+            Array.Resize(ref sortedArr, arr.Length);
+            arr.CopyTo(sortedArr, 0);
+            sortedArr = bubbleSort(sortedArr);
+        }
+
+        public int[] oddEvenSort(int[] list)
+        {
+            int passes = 0;
+            void swap(int[] swaplist, int i, int j)
+            {
+                var temp = swaplist[i];
+                swaplist[i] = swaplist[j];
+                swaplist[j] = temp;
+            }
+            var sorted = false;
+            while (!sorted)
+            {
+                sorted = true;
+                for (var i = 1; i < list.Length - 1; i += 2)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        swap(list, i, i + 1);
+                        sorted = false;
+                    }
+                }
+                for (var i = 0; i < list.Length - 1; i += 2)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        swap(list, i, i + 1);
+                        sorted = false;
+                    }
+                }
+                passes++;
+            }
+            oddEvenPasses = passes;
+            return list;
+        }
+
+        public void sortOddEven()
+        {
+            sortedArr = arr;
+            sortedArr = oddEvenSort(sortedArr);
+        }
+
+        public void printArray()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public void printSortedArray()
+        {
+            for (int i = 0; i < sortedArr.Length; i++)
+            {
+                Console.Write(sortedArr[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public void writeFile(string filename)
+        {
+            try
+            {
+                StreamWriter streamwriter = new StreamWriter(filename);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    streamwriter.WriteLine(arr[i]);
+                }
+                streamwriter.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("error saving to file: " + err.Message);
+            }
+        }
+
+
+    }
+}
